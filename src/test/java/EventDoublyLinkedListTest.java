2778d094d80215a00fa62a05d7c8515291935cf1
virustotalop <virustotalop@gmail.com>
2018-06-23 15:09:21 -0700

    Starting to test removal

--- src/test/java/EventDoublyLinkedListTest.java
+++ src/test/java/EventDoublyLinkedListTest.java
@@ -6,6 +6,7 @@ import com.clubobsidian.trident.EventPriority;
 import com.clubobsidian.trident.MethodExecutor;
 import com.clubobsidian.trident.impl.javaassist.JavaAssistMethodExecutor;
 import com.clubobsidian.trident.util.EventDoublyLinkedList;
+import com.clubobsidian.trident.util.EventNode;
 
 public class EventDoublyLinkedListTest {
 
@@ -21,7 +22,7 @@ public class EventDoublyLinkedListTest {
 			boolean low = list.insert(executor, EventPriority.LOW);
 			assertTrue("Low could not be inserted at head", low);
 			
-			assertTrue("Low was not head", list.getHead().getData().equals(executor));
+			assertTrue("Low is not head", list.getHead().getData().equals(executor));
 			
 			
 			TestListener test2 = new TestListener("test2");
@@ -29,7 +30,7 @@ public class EventDoublyLinkedListTest {
 			boolean lowest = list.insert(executor2, EventPriority.LOWEST);
 			assertTrue("Lowest could not be inserted", lowest);
 			
-			assertTrue("Lowest was not head", list.getHead().getData().equals(executor2));
+			assertTrue("Lowest is not head", list.getHead().getData().equals(executor2));
 			
 			
 			TestListener test3 = new TestListener("test3");
@@ -37,7 +38,7 @@ public class EventDoublyLinkedListTest {
 			boolean nextLowest = list.insert(executor3, EventPriority.LOWEST);
 			assertTrue("NextLowest could not be inserted", nextLowest);
 			
-			assertTrue("NextLowest was not next to head", list.getHead().getNext().getData().equals(executor3));
+			assertTrue("NextLowest is not next to head", list.getHead().getNext().getData().equals(executor3));
 			
 			
 			TestListener test4 = new TestListener("test4");
@@ -53,14 +54,14 @@ public class EventDoublyLinkedListTest {
 			boolean high = list.insert(executor5, EventPriority.HIGH);
 			assertTrue("High could not be inserted", high);
 			
-			assertTrue("High next node was not priority monitor", list.find(executor5).getNext().getData().equals(executor4));
+			assertTrue("High next node is not priority monitor", list.find(executor5).getNext().getData().equals(executor4));
 			
 			TestListener test6 = new TestListener("test6");
 			MethodExecutor executor6 = new JavaAssistMethodExecutor(test6, test6.getClass().getDeclaredMethod("test", TestEvent.class));
 			boolean normal = list.insert(executor6, EventPriority.NORMAL);
 			assertTrue("Normal could not be inserted", normal);
 			
-			assertTrue("Normal next node was not priority high", list.find(executor6).getNext().getData().equals(executor5));
+			assertTrue("Normal next node is not priority high", list.find(executor6).getNext().getData().equals(executor5));
 			
 			
 			TestListener test7 = new TestListener("test7");
@@ -68,8 +69,44 @@ public class EventDoublyLinkedListTest {
 			boolean highest = list.insert(executor7, EventPriority.HIGHEST);
 			assertTrue("Highest could not be inserted", highest);
 			
-			assertTrue("Highest next node was not priority monitor", list.find(executor7).getNext().getData().equals(executor4));
+			assertTrue("Highest next node is not priority monitor", list.find(executor7).getNext().getData().equals(executor4));
+		} 
+		catch (NoSuchMethodException | SecurityException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testRemoval()
+	{	
+		try 
+		{
+			EventDoublyLinkedList list = new EventDoublyLinkedList();
+			TestListener test = new TestListener("test1");
+			MethodExecutor executor = new JavaAssistMethodExecutor(test, test.getClass().getDeclaredMethod("test", TestEvent.class));
+			list.insert(executor, EventPriority.MONITOR);
 			
+			assertTrue("Monitor is not head", list.getHead().getData().equals(executor));
+			list.remove(executor);
+			assertTrue("Head is not null after monitor removal", list.getHead() == null);
+			
+			for(int i = EventPriority.values()[0].getValue(); i < (EventPriority.values()[EventPriority.values().length - 1]).getValue() + 1; i++)
+			{
+				list.insert(new JavaAssistMethodExecutor(new TestListener("test" + i), test.getClass().getDeclaredMethod("test", TestEvent.class)), EventPriority.getByValue(i));
+			}
+			
+			EventNode node = list.getHead();
+			int removalCount = 0;
+			while(node != null)
+			{
+				list.remove(node.getData());
+				removalCount += 1;
+				node = node.getNext();
+			}
+			
+			assertTrue("Head is not null after traversing and removing all node", list.getHead() == null);
+			assertTrue("Removed nodes is not equal to EventPriority length", EventPriority.values().length == removalCount);
 		} 
 		catch (NoSuchMethodException | SecurityException e) 
 		{
