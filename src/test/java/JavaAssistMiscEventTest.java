7d3c62aafdb02db1cf6e08a847333aae0f659a6f
virustotalop <virustotalop@gmail.com>
2018-06-27 15:25:07 -0700

    Event framework test coverage

---
+++ src/test/java/JavaAssistMiscEventTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
+import com.clubobsidian.trident.Event;
+import com.clubobsidian.trident.EventPriority;
+import com.clubobsidian.trident.Listener;
+import com.clubobsidian.trident.MethodExecutor;
+import com.clubobsidian.trident.impl.javaassist.JavaAssistMethodExecutor;
+
+public class JavaAssistMiscEventTest {
+
+	@Test
+	public void eventNameTest()
+	{
+		Event event = new TestEvent();
+		assertTrue(event.getName().equals("TestEvent"));
+	}
+	
+	@Test
+	public void methodExecutorTest()
+	{
+		try 
+		{
+			Listener listener = new TestListener("test");
+			Method testEventMethod = listener.getClass().getDeclaredMethod("test", TestEvent.class);
+			MethodExecutor executor = new JavaAssistMethodExecutor(listener, testEventMethod);
+			
+			assertTrue("Listeners are not equal for method executor", listener.equals(executor.getListener()));
+			assertTrue("Methods are not equal for method executor", testEventMethod.equals(executor.getMethod()));
+		} 
+		catch (NoSuchMethodException | SecurityException e) 
+		{
+			e.printStackTrace();
+		}		
+	}
+	
+	@Test
+	public void eventPriorityTest()
+	{
+		assertTrue("-1 event priority is not null", EventPriority.getByValue(-1) == null);
+		assertTrue("Length event priority is not null", EventPriority.getByValue(EventPriority.values().length) == null);
+	}
+}
