c2c409ce44e2484f85a9fc03f1a6c8d6126ded52
virustotalop <virustotalop@gmail.com>
2018-07-09 23:50:09 -0700

    Moved MultiThreadingTest
    Moved multi-threading tests into new test class.

---
+++ src/test/java/MultiThreadingTest.java
@@ -0,0 +1,60 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Test;
+
+import com.clubobsidian.trident.EventPriority;
+import com.clubobsidian.trident.MethodExecutor;
+import com.clubobsidian.trident.util.EventDoublyLinkedList;
+import com.clubobsidian.trident.util.EventNode;
+import com.clubobsidian.trident.util.JavaAssistUtil;
+
+public class MultiThreadingTest {
+	
+	@Test
+	public void multiThreadingTestEventDoublyLinkedList()
+	{
+		AtomicInteger count = new AtomicInteger(0);
+		final EventDoublyLinkedList list = new EventDoublyLinkedList();
+		Runnable testRunnable = new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				for(int i = 0; i < 1000; i++)
+				{
+					try 
+					{
+						int inc = count.incrementAndGet();
+						TestListener testListener = new TestListener("test" + inc);
+						MethodExecutor testExecutor = JavaAssistUtil.generateMethodExecutor(testListener, testListener.getClass().getDeclaredMethod("test", TestEvent.class));
+						EventNode node = list.insert(testExecutor, EventPriority.LOWEST);
+						assertTrue("Node was null when inserted", node != null);
+					} 
+					catch (NoSuchMethodException | SecurityException e) 
+					{
+						e.printStackTrace();
+					}
+				}
+				Thread.currentThread().interrupt();
+			}
+		};
+		Thread th1 = new Thread(testRunnable);
+		Thread th2 = new Thread(testRunnable);
+		
+		th1.start();
+		th2.start();
+		
+		while(th1.isAlive() || th2.isAlive());
+		
+		int nodeCount = 0;
+		EventNode node = list.getHead();
+		while(node != null)
+		{
+			nodeCount+= 1;
+			node = node.getNext();
+		}
+		assertTrue("Node count not equal when running from multiple thread", nodeCount == count.get());
+	}
+}
