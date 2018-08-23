fa39250ccfa793d389e5bb20f4c339f75ee7daf3
virustotalop <virustotalop@gmail.com>
2018-08-22 17:41:33 -0700

    Finished out documentation for now

--- src/main/java/com/clubobsidian/trident/Cancellable.java
+++ src/main/java/com/clubobsidian/trident/Cancellable.java
@@ -1,7 +1,22 @@
 package com.clubobsidian.trident;
 
+/**
+ * Interface to implement Cancellable. 
+ * Events should implement this interface
+ * if the event should be able to be cancelled.
+ * 
+ * @author virustotalop
+ */
 public interface Cancellable {
 
+	/**
+	 * Returns if the event is cancelled.
+	 * @return if the event is cancelled
+	 */
 	public boolean isCancelled();
+	
+	/**
+	 * Set whether or not for an event to be cancelled.
+	 */
 	public void setCancelled(boolean cancel);
 }
